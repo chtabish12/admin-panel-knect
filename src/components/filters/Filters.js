2701eb303c5f013b80c186022151b92742b68e2b
Tabish <tabish.nadeem@khaleef.com>
2022-01-28 11:38:50 +0500

    update the filters logic by using a method in context

--- src/components/filters/Filters.js
+++ src/components/filters/Filters.js
@@ -3,6 +3,7 @@ import Select from "react-select";
 import "../../components/HeatMapTable/styles.css";
 import "react-datepicker/dist/react-datepicker.css";
 import { MultiSelect } from "react-multi-select-component";
+import { FilterFunction } from "../../context/UserContext";
 
 const Filters = ({ FiltersDisplay, ReportFlag }) => {
   // local
@@ -10,72 +11,26 @@ const Filters = ({ FiltersDisplay, ReportFlag }) => {
   const [productSelect, setProductSelect] = useState([]);
   let serviceArray = [];
   let productArray = [];
-  const LoginApiResp = JSON.parse(localStorage.getItem("api-data"));
-
-  // Product array data for displaying dropdown
-  for (let i = 0; i < LoginApiResp.length; i++)
-    for (let x = 0; x < LoginApiResp[i].products.length; x++) {
-      productArray.push({
-        value: LoginApiResp[i].products[x].id,
-        label: LoginApiResp[i].products[x].name,
-      });
-    } // removing duplicates
+  FilterFunction(
+    productArray,
+    productSelect,
+    serviceArray,
+    serviceSelectValue,
+    FiltersDisplay,
+    ReportFlag
+  );
+  // removing duplicates
   productArray = productArray.filter(
     (value, index, self) =>
       index ===
       self.findIndex((t) => t.label === value.label && t.value === value.value)
   );
-
-  // Service array data for displaying dropdown
-  if (productSelect) {
-    for (let x = 0; x < LoginApiResp.length; x++) {
-      for (let y = 0; y < LoginApiResp[x].products.length; y++)
-        for (let i = 0; i < LoginApiResp[x].products[y].services.length; i++) {
-          if (
-            LoginApiResp[x].products[y].services[i].productId ===
-              productSelect.value &&
-            FiltersDisplay
-          ) {
-            serviceArray.push({
-              value: LoginApiResp[x].products[y].services[i].id,
-              label: LoginApiResp[x].products[y].services[i].name,
-            });
-          } else if (productSelect.length && ReportFlag)
-            for (let j = 0; j < productSelect.length; j++)
-              if (
-                LoginApiResp[x].products[y].services[i].productId ===
-                productSelect[j].value
-              ) {
-                serviceArray.push({
-                  ...serviceArray,
-                  value: LoginApiResp[x].products[y].services[i].id,
-                  label: LoginApiResp[x].products[y].services[i].name,
-                });
-              }
-        }
-    }
-    serviceArray = serviceArray.filter(
-      (value, index, self) =>
-        index ===
-        self.findIndex(
-          (t) => t.label === value.label && t.value === value.value
-        )
-    );
-  }
-  if (FiltersDisplay) {
-    localStorage.setItem("service-data", serviceSelectValue.value);
-  } else if (ReportFlag) {
-    let productArrayValue = [];
-    let serviceArrayValue = [];
-    for (let x = 0; x < productSelect.length; x++) {
-      productArrayValue.push(productSelect[x].value);
-    }
-    for (let x = 0; x < serviceSelectValue.length; x++) {
-      serviceArrayValue.push(serviceSelectValue[x].value);
-    }
-    localStorage.setItem("product-data", productArrayValue.join(","));
-    localStorage.setItem("service-data", serviceArrayValue.join(","));
-  }
+  // removing duplicates
+  serviceArray = serviceArray.filter(
+    (value, index, self) =>
+      index ===
+      self.findIndex((t) => t.label === value.label && t.value === value.value)
+  );
 
   return (
     <>
--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -44,7 +44,71 @@ const useUserDispatch = () => {
   return context;
 };
 
-// ###########################################################
+//////////////////////##FILTER METHOD###/////////////////////
+const FilterFunction = (
+  productArray,
+  productSelect,
+  serviceArray,
+  serviceSelectValue,
+  FiltersDisplay,
+  ReportFlag
+) => {
+  const LoginApiResp = JSON.parse(localStorage.getItem("api-data"));
+
+  // Product array data for displaying dropdown
+  for (let i = 0; i < LoginApiResp.length; i++)
+    for (let x = 0; x < LoginApiResp[i].products.length; x++) {
+      productArray.push({
+        value: LoginApiResp[i].products[x].id,
+        label: LoginApiResp[i].products[x].name,
+      });
+    }
+
+  // Service array data for displaying dropdown
+  if (productSelect) {
+    for (let x = 0; x < LoginApiResp.length; x++) {
+      for (let y = 0; y < LoginApiResp[x].products.length; y++)
+        for (let z = 0; z < LoginApiResp[x].products[y].services.length; z++) {
+          if (
+            LoginApiResp[x].products[y].services[z].productId ===
+              productSelect.value &&
+            FiltersDisplay
+          ) {
+            serviceArray.push({
+              value: LoginApiResp[x].products[y].services[z].id,
+              label: LoginApiResp[x].products[y].services[z].name,
+            });
+          } else if (productSelect.length && ReportFlag)
+            for (let j = 0; j < productSelect.length; j++)
+              if (
+                LoginApiResp[x].products[y].services[z].productId ===
+                productSelect[j].value
+              ) {
+                serviceArray.push({
+                  ...serviceArray,
+                  value: LoginApiResp[x].products[y].services[z].id,
+                  label: LoginApiResp[x].products[y].services[z].name,
+                });
+              }
+        }
+    }
+  }
+  if (FiltersDisplay) {
+    localStorage.setItem("service-data", serviceSelectValue.value);
+  } else if (ReportFlag) {
+    let productArrayValue = [];
+    let serviceArrayValue = [];
+    for (let x = 0; x < productSelect.length; x++) {
+      productArrayValue.push(productSelect[x].value);
+    }
+    for (let x = 0; x < serviceSelectValue.length; x++) {
+      serviceArrayValue.push(serviceSelectValue[x].value);
+    }
+    localStorage.setItem("product-data", productArrayValue.join(","));
+    localStorage.setItem("service-data", serviceArrayValue.join(","));
+  }
+};
+//////////////////////##LOGIN METHOD###/////////////////////
 const loginUser = (dispatch, history, setIsLoading, message) => {
   if (message === 200) {
     sessionStorage.setItem("id_token", 1);
@@ -52,11 +116,10 @@ const loginUser = (dispatch, history, setIsLoading, message) => {
     dispatch({ type: "LOGIN_SUCCESS" });
     history.push("/app/dashboard");
   } else if (message === 400) {
-    // console.log(" error !!!!!!");
     setIsLoading(false);
   }
 };
-
+//////////////////////##LOGOUT METHOD###/////////////////////
 const signOut = (dispatch, history) => {
   sessionStorage.removeItem("id_token");
   sessionStorage.removeItem("token-user");
@@ -66,4 +129,11 @@ const signOut = (dispatch, history) => {
   localStorage.clear();
 };
 
-export { UserProvider, useUserState, useUserDispatch, loginUser, signOut };
+export {
+  UserProvider,
+  useUserState,
+  useUserDispatch,
+  FilterFunction,
+  loginUser,
+  signOut,
+};
